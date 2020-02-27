0766272274edb6178dc9313c5a227d91f640b66c
taterntots <matthew.a.weidner@gmail.com>
2020-02-27 12:56:44 -0500

    google analytics should display how many users per page, if done correctly

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 // React Router
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 // Redux
 import { connect } from 'react-redux';
 // Google Analytics
 import ReactGA from 'react-ga';
+import { createBrowserHistory } from 'history';
 // Components
 import Login from './components/Login';
 import NavBar from './components/NavBar';
@@ -17,12 +18,22 @@ import './App.css';
 
 function initializeReactGA() {
   ReactGA.initialize('UA-159232862-1');
-  ReactGA.pageview('/homepage');
+  ReactGA.pageview(window.location.pathname + window.location.search);
 }
 
-initializeReactGA();
+// const history = createBrowserHistory();
+
+// Initialize google analytics page view tracking
+// history.listen(location => {
+//   ReactGA.set({ page: location.pathname }); // Update the user's current page
+//   ReactGA.pageview(location.pathname); // Record a pageview for the given page
+// });
 
 const App = () => {
+  useEffect(() => {
+    initializeReactGA();
+  }, [])
+
   return (
     <Router>
       <main className="App">
