e5da5d802af2e711a41bc16456c244555e64acfd
taterntots <matthew.a.weidner@gmail.com>
2020-02-27 13:07:37 -0500

    added an event listener for google analytics. pushing the add to team button

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,17 +1,27 @@
 import React, { useState } from 'react';
+import ReactGA from 'react-ga';
 import { connect } from 'react-redux';
 import { CardStyle } from './Styles';
 import Button from '@material-ui/core/Button';
 
 const PokemonCard = (props) => {
   console.log(props.name);
+
+  const ClickHandler = () => {
+    ReactGA.event({
+      category: 'Button',
+      action: 'Click the button to add a Pokemon to the team'
+    })
+    alert('The information was sent to google analytics that this button was clicked');
+  }
+
   return (
     <div>
       <CardStyle>
         <h2>Dex# {props.dexNum}</h2>
         <h2>{props.name}</h2>
         <h2>{props.type1}</h2>
-        <Button variant='contained' color='primary'>Add to Team</Button>
+        <Button onClick={ClickHandler} variant='contained' color='primary'>Add to Team</Button>
       </CardStyle>
     </div>
   )
